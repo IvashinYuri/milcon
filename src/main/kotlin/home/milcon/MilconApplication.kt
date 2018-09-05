3475e55bc12f17762d46a9ee62c34241f7cf3c47
IvashinYuri <zplata@yandex.ru>
2018-09-05 23:23:12 +1000

    last commit

--- src/main/kotlin/home/milcon/MilconApplication.kt
+++ src/main/kotlin/home/milcon/MilconApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class MilconApplication
+class MilconApplication {}
 
 fun main(args: Array<String>) {
     SpringApplication.run(MilconApplication::class.java, *args)
